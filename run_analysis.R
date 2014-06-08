38e4acb826946b6b11e162bd69dad611ca3cbcee
Alex Pittendreigh <alexp@numbatmarketing.com>
2014-06-08 18:23:28 +0800

    Initial cut of the utilities. Start of the checkOrGetFiles function.

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@
 #         Written by: Alex Pittendreigh
 #               Date: 8th June 2014
 # Relies on Packages:
-#      Other Modules:
+#      Other Modules: utilities.R
 #            Remarks: This is the main R driver program that will download,
 #                   : extract, and process data files as required by the
 #                   : project specification for the "Getting and Cleaning 
@@ -14,8 +14,15 @@
 # Load required packages
 
 # Load required supplmentary modukes
+source("utilities.R")
 
 # Mainline - Entry point for the module
 run_analysis <- function() {
+    # Name of the data files
+    RAW_DATA_FILES <- c()
     
+    # If the files don't exist and/or cant be retrieved
+    if ( !checkOrGetFiles( RAW_DATA_FILES )) {
+        stop( "Cannot access raw data files." )    # Exit with error message
+    }
 }
---
+++ utilities.R
@@ -0,0 +1,41 @@
+#-----------------------------------------------------------------------------
+#             Module: utilities.R
+#         Written by: Alex Pittendreigh
+#               Date: 8th June 2014
+# Relies on Packages:
+#      Other Modules:
+#            Remarks: This is the file of supplementary routines required by 
+#                   : the run_analysis.R module.
+#                   :
+#                   : It contains functions that are needed to do the house-
+#                   : keeping rather than having them embedded in the main 
+#                   : module which controlls the processing. This module
+#                   : has routines for creating directories and files, 
+#                   : downloading of files, unarchiving files etc. Each
+#                   : function has a short description to it's purpose.
+#-----------------------------------------------------------------------------
+
+#   Function: checkOrGetFiles
+#      Notes: This iteration of the function simply checks to see if the named 
+#           : file is located in the working directory. If this is the case  
+#           : then returns TRUE else will attempt to download and extract the 
+#           : file. Can be enhanced for better error checking, date checks etc.
+# Parameters: filesToCheck - chara array with names of the requested files
+#    Returns: boolean indicating if the files exists or not
+checkOrGetFiles <- function( filesToCheck ) {
+    allOK <- TRUE
+    
+    # cycle through the names checking to make sure that all exist
+    for ( fileName in filesToCheck ) {
+        if ( !file.exists( fileName )) {
+            allOK <- FALSE                # current file doesn't exist
+        }
+    }
+    
+    # If at least one of the named files doesn't exist
+    if ( !allOK ) {
+        # attampt to go get the files
+    }
+    
+    return( allOK )
+}
